2f97b8aa8a57d42915304b8c46d82eefd26ff56d
btb18131 <quaQue6ezaev>
2021-05-26 14:11:11 +0100

    Colour update

--- indexScript.js
+++ indexScript.js
@@ -2,12 +2,14 @@
 
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("side").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
+    setTimeout(() => {document.getElementById("smallSide").style.display = "none";}, 100)
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("side").style.width = "0";
+    document.getElementById("main").style.marginLeft = "50px";
+    setTimeout(() => {document.getElementById("smallSide").style.display = "block";}, 200)
 }
