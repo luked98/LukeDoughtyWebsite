d68eee32bc64c26312a8e05d728e4a6c2acf56c4
btb18131 <quaQue6ezaev>
2021-05-27 16:35:39 +0100

    Update art section

--- indexScript.js
+++ indexScript.js
@@ -13,3 +13,38 @@ function closeNav() {
     document.getElementById("main").style.marginLeft = "50px";
     setTimeout(() => {document.getElementById("smallSide").style.display = "block";}, 200)
 }
+
+let st = [1, 21, 31];
+let nd = [2, 22];
+let rd = [3, 23];
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+function dateOrdinal(d) {
+    if (st.includes(d)) {
+        return "st";
+    } else if (nd.includes(d)) {
+        return "nd";
+    } else if (rd.includes(d)) {
+        return "rd";
+    } else {
+        return "th";
+    }
+}
+
+function setDate() {
+    let dt = new Date();
+
+    let day = days[dt.getDay()];
+    let date = dt.getDate();
+    let month = months[dt.getMonth()];
+    let year = dt.getFullYear();
+
+    document.getElementById("dateTime").innerHTML = day + " " + date + dateOrdinal(date) + " " + month + " " + year;
+}
+
+let initTime = () => {
+    setDate();
+}
+
+window.addEventListener("load", initTime)
